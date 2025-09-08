6b94824f7a4f2ef397a370375d879ae77874febc
ohyzz <artemyt216@gmail.com>
2025-09-09 01:22:04 +0800

    Create script.js
    added a lot

---
+++ js/script.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const verticalBars = document.getElementById('verticalBars');
+    const sideMenu = document.getElementById('sideMenu');
+    const closeMenu = document.getElementById('closeMenu');
+    const CartBtn = document.getElementById('cartbtn')
+    
+    verticalBars.addEventListener('click', function() {
+        sideMenu.classList.add('active');
+    });
+    
+    closeMenu.addEventListener('click', function() {
+        sideMenu.classList.remove('active');
+    });
+    
+
+    document.addEventListener('click', function(event) {
+        if (sideMenu.classList.contains('active') && 
+            !sideMenu.contains(event.target) && 
+            !verticalBars.contains(event.target)) {
+            sideMenu.classList.remove('active');
+            document.body.style.overflow = '';
+        }
+    });
+    
+    const menuButtons = document.querySelectorAll('.menu-btn');
+    menuButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const targetUrl = this.getAttribute('data-url');
+            
+            if (targetUrl) {
+                document.body.style.opacity = '0';
+                document.body.style.transition = 'opacity 0.5s ease';
+                
+                setTimeout(() => {
+                    window.location.href = targetUrl;
+                }, 500);
+            }
+        });
+    });
+
+    CartBtn.addEventListener('click', function(event) {
+        const targetUrl = this.getAttribute('data-url');
+        
+        if (targetUrl) {
+            document.body.style.opacity = '0';
+            document.body.style.transition = 'opacity 0.5s ease';
+                
+            setTimeout(() => {
+                window.location.href = targetUrl;
+            }, 500);
+        }
+    });
+    
+
+    sideMenu.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+});
